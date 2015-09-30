5e94683b1fb0279a072bbb86b2ce413eb6ac4131
Brian Lau & Corrie Smith <student@epicodus.com>
2015-09-30 09:54:43 -0700

    add json file and firebase database

--- app/routes/bootcamp.js
+++ app/routes/bootcamp.js
@@ -1,21 +1,7 @@
 import Ember from 'ember';
 
-var bootcamps = [{
-  name: "Code Fellows",
-  location: "Seattle, WA",
-  description: "Description"
-}, {
-  name: "Epicodus",
-  location: "Portland, OR",
-  description: "Description"
-}, {
-  name: "Codeup",
-  location: "San Antonio, TX",
-  description: "Description"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return bootcamps;
+    return this.store.findAll('bootcamp');
   },
 });
